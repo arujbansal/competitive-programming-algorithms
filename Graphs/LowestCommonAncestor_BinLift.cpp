b04aa3fac85187ba55af23f3f4215ecae3d3f4a5
Aruj Bansal <arujbansal1@gmail.com>
2020-10-06 17:10:43 +0530

    Updated Binary Lifting LCA

--- Graphs/LowestCommonAncestor_BinLift.cpp
+++ Graphs/LowestCommonAncestor_BinLift.cpp
@@ -1,23 +1,23 @@
-struct BinLiftLCA {
+struct LCA {
     int n, k, timer;
-    vi tin, tout;
-    vector<vector<int>> par;
+    vector<int> tin, tout;
+    vector<vector<int>> par, g;
 
     void init(int x) {
         n = x;
-        k = ceil(log2(n));
+        k = 31 - __builtin_clz(n);
         timer = 0;
         tin.resize(n);
         tout.resize(n);
         par.assign(n, vector<int>(k + 1, 0));
-        dfs();
+        g.resize(n + 1);
     }
 
     void dfs(int u = 0, int p = -1) {
         tin[u] = timer++;
         par[u][0] = (p == -1 ? 0 : p);
         for (int j = 1; j <= k; j++) par[u][j] = par[par[u][j - 1]][j - 1];
-        trav(v, g[u]) if (v != p) dfs(v, u);
+        for (const auto &v : g[u]) if (v != p) dfs(v, u);
         tout[u] = timer - 1;
     }
 
