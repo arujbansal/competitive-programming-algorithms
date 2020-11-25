b6834f5d7f7e935811cef33aa79b8b3de72f8636
Aruj Bansal <arujbansal1@gmail.com>
2020-11-25 14:55:43 +0530

    Improved Sparse Table

--- Data Structures/SparseTable.cpp
+++ Data Structures/SparseTable.cpp
@@ -1,22 +1,27 @@
+template<bool isMx>
 struct SparseTable {
     int n, k;
     vector<int> a;
     vector<vector<int>> st;
 
-    void init(int x, vector<int> y) {
-        n = x;
+    void init(vector<int> a) {
+        this->a = a;
+        n = (int) a.size();
         k = lg(n);
-        a = y;
         st.assign(n + 1, vector<int>(k + 1));
         build();
     }
 
     int lg(int x) { return 31 - __builtin_clz(x); }
 
-    int merge(int x, int y) { return a[x] < a[y] ? x : y; }
+    int merge(int x, int y) {
+        if (isMx) return a[x] > a[y] ? x : y;
+        return a[x] < a[y] ? x : y;
+    }
 
     void build() {
         for (int i = 0; i < n; i++) st[i][0] = i;
+
         for (int j = 1; j <= k; j++)
             for (int i = 0; i + (1 << j) - 1 < n; i++)
                 st[i][j] = merge(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
