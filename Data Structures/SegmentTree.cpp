605ab261f8beb69845b693e5de4f066f455dec55
Aruj Bansal <arujbansal1@gmail.com>
2021-03-04 10:09:12 +0530

    New segment tree, added 2-SAT

--- Data Structures/SegmentTree.cpp
+++ Data Structures/SegmentTree.cpp
@@ -1,53 +1,84 @@
-struct SegmentTree {
+template<typename T>
+struct segment_tree {
     int n;
-    vector<int> a, t;
+    vector<T> a, tree;
+    T identity;
+    function<T(const T&, const T&)> merge;
 
-    void init(int x) {
-        n = x;
-        t.assign(4 * n, 0);
-    }
+    void init(const vector<T> &a, T identity, const function<T(const T&, const T&)> &merge) {
+        n = (int) a.size();
+        tree.resize(4 * n);
 
-    void init(int x, vector<int> y) {
-        n = x;
-        a = move(y);
-        t.resize(4 * n);
+        this->a = a;
+        this->identity = identity;
+        this->merge = merge;
+
         build(1, 0, n - 1);
     }
 
-    int merge(int x, int y) { return x + y; }
+    void init(int n, T identity, const function<T(const T&, const T&)> &merge) {
+        this->n = n;
+        this->identity = identity;
+        this->merge = merge;
 
-    int upd(int x, int y) { return y; }
+        tree.assign(4 * n, identity);
+    }
 
     void build(int i, int l, int r) {
         if (l == r) {
-            t[i] = a[l];
+            tree[i] = a[l];
             return;
         }
-        int mid = (l + r) >> 1;
-        build(lc(i), l, mid);
-        build(rc(i), mid + 1, r);
-        t[i] = merge(t[lc(i)], t[rc(i)]);
+
+        int mid = (l + r) / 2;
+        build(2 * i, l, mid);
+        build(2 * i + 1, mid + 1, r);
+
+        tree[i] = merge(tree[2 * i], tree[2 * i + 1]);
     }
 
-    void modify(int i, int l, int r, int pos, int val) {
+    void identity_modify(int i, int l, int r, int pos, T val) {
         if (l == r) {
-            t[i] = upd(t[i], val);
+            tree[i] = merge(tree[i], val);
             return;
         }
-        int mid = (l + r) >> 1;
-        if (pos <= mid) modify(lc(i), l, mid, pos, val);
-        else modify(rc(i), mid + 1, r, pos, val);
-        t[i] = merge(t[lc(i)], t[rc(i)]);
+
+        int mid = (l + r) / 2;
+        if (pos <= mid) identity_modify(2 * i, l, mid, pos, val);
+        else identity_modify(2 * i + 1, mid + 1, r, pos, val);
+
+        tree[i] = merge(tree[2 * i], tree[2 * i + 1]);
     }
 
-    int query(int i, int l, int r, int ql, int qr) {
-        if (l > qr || r < ql) return 0;
-        if (l >= ql && r <= qr) return t[i];
-        int mid = (l + r) >> 1;
-        return merge(query(lc(i), l, mid, ql, qr), query(rc(i), mid + 1, r, ql, qr));
+    void increment(int i, int l, int r, int pos, T val) {
+        if (l == r) {
+            tree[i] += val;
+            return;
+        }
+
+        int mid = (l + r) / 2;
+        if (pos <= mid) increment(2 * i, l, mid, pos, val);
+        else increment(2 * i + 1, mid + 1, r, pos, val);
+
+        tree[i] = merge(tree[2 * i], tree[2 * i + 1]);
     }
 
-    void modify(int pos, int val) { modify(1, 0, n - 1, pos, val); }
+    T query(int i, int l, int r, int ql, int qr) {
+        if (l > qr || r < ql) return identity;
+        if (l >= ql && r <= qr) return tree[i];
 
-    int query(int l, int r) { return query(1, 0, n - 1, l, r); }
+        int mid = (l + r) / 2;
+        T resL = query(2 * i, l, mid, ql, qr);
+        T resR = query(2 * i + 1, mid + 1, r, ql, qr);
+
+        return merge(resL, resR);
+    }
+
+    void identity_modify(int pos, T val) { identity_modify(1, 0, n - 1, pos, val); }
+
+    void increment(int pos, T val) { increment(1, 0, n - 1, pos, val); }
+
+    T query(int pos) { return query(1, 0, n - 1, pos, pos); }
+
+    T query(int l, int r) { return query(1, 0, n - 1, l, r); }
 };
