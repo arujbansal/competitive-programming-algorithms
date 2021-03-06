bef08d62271ee1221ae58e20200b13b9d9878029
Aruj Bansal <arujbansal1@gmail.com>
2021-03-06 19:21:41 +0530

    Updated Li Chao Tree

--- Data Structures/LiChaoTree.cpp
+++ Data Structures/LiChaoTree.cpp
@@ -1,71 +1,73 @@
-template<typename T>
-struct LiChaoTree {
+template<typename T> 
+struct li_chao_min {
+    static const T identity = numeric_limits<T>::max();
 
     struct Line {
-        T slope, yIntercept;
+        T m, c;
 
         Line() {
-            slope = 0;
-            yIntercept = INF;
+            m = 0;
+            c = identity;
         }
 
-        Line(T slope, T yIntercept) : slope(slope), yIntercept(yIntercept) {}
+        Line(T m, T c) : m(m), c(c) {}
 
-        T val(T x) {
-            return slope * x + yIntercept;
-        }
+        T val(T x) { return m * x + c; }
     };
 
     struct Node {
-        int left, right;
         Line line;
+        Node *lc, *rc;
 
-        Node() {
-            left = -1;
-            right = -1;
-        }
-
-        T val(T x) { return line.val(x); }
+        Node() : lc(0), rc(0) {}        
     };
 
-    T n;
-    vector<Node> t;
+    T L, R, eps;
+    deque<Node> buffer;
+    Node* root;
 
-    void init(T n) {
-        this->n = n;
-        t.clear();
-        t.emplace_back();
-        t.emplace_back();
+    Node* new_node() {
+        buffer.emplace_back();
+        return &buffer.back();
     }
 
-    void create_children(int i) {
-        if (t[i].left > -1) return;
-        t[i].left = sz(t);
-        t.emplace_back();
-        t[i].right = sz(t);
-        t.emplace_back();
+    void init(T L, T R, T eps) {
+        this->L = L;
+        this->R = R;
+        this->eps = eps;
+
+        root = new_node();
     }
 
-    void insert(int i, T l, T r, Line newLine) {
-        T mid = (l + r) >> 1;
-        if (newLine.val(mid) < t[i].val(mid)) swap(newLine, t[i].line);
-        if (r - l <= 1) return;
-        if (newLine.yIntercept >= INF) return;
-        create_children(i);
-        if (newLine.val(l) <= t[i].val(l)) insert(t[i].left, l, mid, newLine);
-        else insert(t[i].right, mid + 1, r, newLine);
+    void insert(Node* &cur, T l, T r, Line line) {
+        if (!cur) {
+            cur = new_node();
+            cur->line = line;
+            return;
+        }
+
+        T mid = l + (r - l) / 2;
+        if (r - l <= eps) return;
+
+        if (line.val(mid) < cur->line.val(mid))
+            swap(line, cur->line);
+
+        if (line.val(l) < cur->line.val(l)) insert(cur->lc, l, mid, line);
+        else insert(cur->rc, mid + 1, r, line);
     }
 
-    T query(int i, T l, T r, T x) {
-        if (r - l <= 1) return t[i].val(x);
-        if (t[i].line.yIntercept >= INF) return INF;
-        create_children(i);
-        T mid = (l + r) >> 1;
-        if (x <= mid) return min(t[i].val(x), query(t[i].left, l, mid, x));
-        else return min(t[i].val(x), query(t[i].right, mid + 1, r, x));
+    int query(Node* &cur, T l, T r, T x) {
+        if (!cur) return identity;
+
+        T mid = l + (r - l) / 2;
+        T res = cur->line.val(x);
+        if (r - l <= eps) return res;
+
+        if (x <= mid) return min(res, query(cur->lc, l, mid, x));
+        else return min(res, query(cur->rc, mid + 1, r, x));
     }
 
-    void insert(T slope, T yIntercept) { insert(1, 0, n - 1, Line(slope, yIntercept)); }
+    void insert(T m, T c) { insert(root, L, R, Line(m, c)); }
 
-    T query(T x) { return query(1, 0, n - 1, x); }
+    int query(T x) { return query(root, L, R, x); }
 };
